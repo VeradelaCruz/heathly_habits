b94129f7beeaa7326079354f0175c4c5bf0a9ecb
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-08-17 08:37:35 +0200

    Probando automatic push 1.3

--- habit-service/src/main/java/com/example/habit_service/client/UserClientService.java
+++ habit-service/src/main/java/com/example/habit_service/client/UserClientService.java
@@ -6,6 +6,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
+//::
 public class UserClientService {
 
     private final WebClient webClient;
